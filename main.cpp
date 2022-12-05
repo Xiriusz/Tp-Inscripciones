a629d612dadc08cb353088516712a0c5a11494f2
Tom.github <86371493+Xiriusz@users.noreply.github.com>
2022-12-05 11:05:50 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <conio.h>
+using namespace std;
+
+struct Alumnos{
+    int Legajo;
+    char NomyAp[30];
+    int Antiguedad;
+};
+struct Materias{
+    int CodM;
+    char Nombre[30];
+    int Capacidad;
+};
+struct Inscrip{
+    int Legajo;
+    int CodM;
+};
+struct PromAntiguedad{
+    int CodM;
+    int Antiguedad;
+    char NombreM[30];
+    char NombreA[30];
+    int Capacidad;
+    int Legajo;
+};
+struct Nodo {
+    Nodo *sig;
+    Alumnos NodoAlum;
+};
+
+
+void genAlumnos();
+void lecAlumnos(); // Hacerlo lindo
+void genMaterias();
+void lecMaterias();// Hacerlo lindo
+void genInscrip();
+void lecInscrip();// Hacerlo lindo
+
+int main() {
+    int resp;
+    std::cout << "Hello, World!" << std::endl;
+    cout << "Bienvenido, por favor seleccione la accion deseada \n";
+    cout << "Opcion 1) Ingresar datos de alumnos\n";
+    cout << "Opcion 2) Leer datos de alumnos \n";
+    cout << "Opcion 3) Ingresar datos de Materias \n";
+    cout << "Opcion 4) Leer datos de Materias \n";
+    cout << "Opcion 5) Ingresar datos de Inscripciones \n";
+    cout << "Opcion 6) Leer datos de Inscripciones \n";
+
+    cin >> resp;
+    switch (resp) {
+        case 1:
+            genAlumnos();
+            break;
+        case 2:
+            lecAlumnos();
+            break;
+        case 3:
+            genMaterias();
+            break;
+        case 4:
+            lecMaterias();
+            break;
+        case 5:
+            genInscrip();
+            break;
+        case 6:
+            lecInscrip();
+            break;
+    }
+
+    return 0;
+}
+
+// Generacion de Archivos y Lectura
+void genAlumnos(){
+    Alumnos aPersona;
+    FILE *Alumnos;
+    Alumnos= fopen("Alumnos.dat","wb");
+        cout << "Ingrese legajo ( 0 para detener la grabacion de datos) \n";
+        cin >> aPersona.Legajo;
+        while(aPersona.Legajo != 0) {
+            cout << "Ingrese Nombre " << endl;
+            cin >> aPersona.NomyAp;
+            cout << "Ingrese antiguedad" << endl;
+            cin >> aPersona.Antiguedad;
+            fwrite(&aPersona, sizeof(aPersona), 1, Alumnos);
+            cout << "Ingrese legajo ( 0 para detener la grabacion de datos)" << endl;
+            cin >> aPersona.Legajo;
+
+        }
+        fclose(Alumnos);
+        cout << "Datos ingresados correctamente" << endl;
+
+}
+void lecAlumnos(){
+    Alumnos aPersona;
+    FILE *Alumnos;
+    Alumnos = fopen("Alumnos.dat", "rb");
+    if (Alumnos == NULL){
+        cout << "No se puede abrir el archivo \n";
+}
+    cout << "Listado de alumnos de sistemas \n";
+    cout << "Legajo" << " " << "Nombre" << " " << "Antiguedad " <<"\n";
+    fread(&aPersona,sizeof(aPersona),1,Alumnos);
+    while (!feof(Alumnos)){
+    cout << aPersona.Legajo << "     " << aPersona.NomyAp << "     " << aPersona.Antiguedad << "\n";
+        fread(&aPersona,sizeof(aPersona),1,Alumnos);
+    }
+    fclose(Alumnos);
+    cout << "Lectura completada" << endl;
+}
+void genMaterias(){
+    Materias aMateria;
+    FILE *Materias;
+    Materias= fopen("Materias.dat","wb");
+        cout << "Ingrese codigo de la materia ( 0 para detener la grabacion de datos)" << endl;
+        cin >> aMateria.CodM;
+    while(aMateria.CodM != 0){
+        cout<<"Ingrese nombre de la materia "<< endl;
+        cin>>aMateria.Nombre;
+        cout<<"Ingrese capacidad" <<endl;
+        cin>>aMateria.Capacidad;
+        fwrite(&aMateria,sizeof(aMateria),1,Materias);
+        cout << "Ingrese codigo de la materia ( 0 para detener la grabacion de datos) \n";
+        cin >> aMateria.CodM;
+    }
+    fclose(Materias);
+    cout << "Datos ingresados correctamente" << endl;
+}
+void lecMaterias(){
+    Materias aMateria;
+    FILE *Materias;
+    Materias = fopen("Materias.dat", "rb");
+    if (Materias == NULL){
+        cout << "No se puede abrir el archivo \n";
+    }
+    cout << "Listado de alumnos de sistemas \n";
+    cout << "Codigo" << " " << "Nombre" << " " << "Capacidad" <<"\n";
+    fread(&aMateria,sizeof(aMateria),1,Materias);
+    while (!feof(Materias)){
+        cout << aMateria.CodM << "     " << aMateria.Nombre << "     " << aMateria.Capacidad << "\n";
+        fread(&aMateria,sizeof(aMateria),1,Materias);
+    }
+    fclose(Materias);
+    cout << "Lectura completada" << endl;
+}
+void genInscrip(){
+    Inscrip aInscrip;
+    FILE *Inscripciones;
+    Inscripciones= fopen("Inscripciones.dat","wb");
+        cout << "Ingrese legajo ( 0 para detener la grabacion de datos) \n";
+        cin >> aInscrip.Legajo;
+     while(aInscrip.Legajo != 0){
+        cout<<"Ingrese Codigo de la materia "<<endl;
+        cin>>aInscrip.CodM;
+        fwrite(&aInscrip,sizeof(aInscrip),1,Inscripciones);
+         cout << "Ingrese legajo ( 0 para detener la grabacion de datos) "<<endl;
+         cin >> aInscrip.Legajo;
+    }
+
+    fclose(Inscripciones);
+    cout << "Datos ingresados correctamente" << endl;
+}
+void lecInscrip(){
+    FILE *Inscripciones;
+    Inscrip aInscrip;
+    Inscripciones = fopen("Inscripciones.dat", "rb");
+    if (Inscripciones == NULL){
+        cout << "No se puede abrir el archivo \n";
+    }
+    cout << "Listado de alumnos de sistemas \n";
+    cout << "Legajo" << " " << "Materia" << " " <<"\n";
+    fread(&aInscrip,sizeof(aInscrip),1,Inscripciones);
+    while (!feof(Inscripciones)){
+        cout << aInscrip.Legajo << "     " << aInscrip.CodM << "     " << "\n";
+        fread(&aInscrip,sizeof(aInscrip),1,Inscripciones);
+    }
+    fclose(Inscripciones);
+    cout << "Lectura completada" << endl;
+}
+// Busquedas Secuenciales
+bool BuscSecuAlum(int numBusq, vector <Alumnos> alumno) {
+    bool band = false;
+    for (int i = 0; i < alumno.size(); ++i) {
+        if (alumno[i].Legajo == numBusq){
+            band = true;
+            break;
+        }
+    }
+    return band;
+}
+bool BuscSecuMat(int numBusq, vector <Materias> materia) {
+    bool band = false;
+    for (int i = 0; i < materia.size(); ++i) {
+        if (materia[i].CodM == numBusq){
+            band = true;
+            break;
+        }
+    }
+    return band;
+}
+bool busqSecuInsc(int numBusq, vector<Inscrip> inscripciones) {
+    bool band = false;
+    for (int i = 0; i < inscripciones.size(); ++i) {
+        if (inscripciones[i].CodM == numBusq){
+            band = true;
+            break;
+        }
+    }
+    return band;
+
+}
+// Funciones para Informe
+int matSinInscrip( vector<Inscrip> inscripciones, vector<Materias> materias) {
+    int materiasSinInscriptos = 0;
+    for (int i = 0; i < materias.size(); i++) {
+        if(!busqSecuInsc(materias[i].CodM, inscripciones))
+            materiasSinInscriptos++;
+    }
+
+    return materiasSinInscriptos;
+}
+float promAntig( vector<Inscrip> inscripciones, vector<PromAntiguedad> antiguedad) {
+    float prom;
+    for (int i = 0; i < inscripciones.size(); i++) {
+        inscripciones[i].CodM = antiguedad[i].CodM;
+        inscripciones[i].Legajo;
+
+    }
+}
+void ordenLista (Nodo *&Lista, Alumnos RAlu){
+        Nodo *pnuevo = new Nodo();
+        pnuevo->NodoAlum = RAlu;
+
+        Nodo *p2 = Lista;
+        Nodo *p3;
+
+    while ((p2 != NULL) && (p2->NodoAlum.NomyAp < RAlu.NomyAp)){
+        p2 = p3;
+        p2 = p2->sig;
+    }
+    if (Lista == p2){
+        Lista = pnuevo;
+    } else{
+        p3->sig = pnuevo;
+    }
+    pnuevo->sig = p2;
+}
